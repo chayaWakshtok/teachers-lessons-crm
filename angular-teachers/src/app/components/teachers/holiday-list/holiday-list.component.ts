bc3e920d8597400eea672eee953cf3d737e4ea71
chaya <c0556777462@gmail.com>
2023-06-04 13:52:22 +0300

    angular holiday1

--- angular-teachers/src/app/components/teachers/holiday-list/holiday-list.component.ts
+++ angular-teachers/src/app/components/teachers/holiday-list/holiday-list.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IconSetService } from '@coreui/icons-angular';
+import { brandSet, flagSet, freeSet } from '@coreui/icons';
+import { Router } from '@angular/router';
+import { Lesson } from 'src/app/models/lesson';
+import { AccountService } from 'src/app/services/account.service';
+import { User } from 'src/app/models/user';
+import { LessonService } from 'src/app/services/lesson.service';
 
 @Component({
   selector: 'app-holiday-list',
@@ -6,5 +13,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./holiday-list.component.scss']
 })
 export class HolidayListComponent {
+  lessons: Lesson[] = [];
+  user: User | null | undefined;
 
+  constructor(
+    public router: Router,
+    public iconSet: IconSetService,
+    public accountService: AccountService,
+    public lessonService: LessonService
+  ) {
+    iconSet.icons = { ...freeSet, ...brandSet, ...flagSet };
+  }
+
+  ngOnInit(): void {
+    this.accountService.user.subscribe(x => {
+      this.user = x;
+
+      this.lessonService.getAllByTeacher(this.user?.teacher?.id ?? 0).subscribe((res: any) => {
+        this.lessons = res;
+      })
+    });
+  }
+
+  addNew() {
+    this.router.navigate(["teacher/holiday"])
+  }
+
+  edit(id: number) {
+    this.router.navigate(["teacher/holiday", { id: id }])
+  }
 }
