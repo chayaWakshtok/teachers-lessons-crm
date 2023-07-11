3c5f0021ef6f3dee78a340bd4de21443893a1782
chayaWakshtok <38976767+chayaWakshtok@users.noreply.github.com>
2023-07-11 18:25:56 +0300

    fix2

--- angular-teachers/src/app/components/default-header/default-header.component.ts
+++ angular-teachers/src/app/components/default-header/default-header.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { ClassToggleService, HeaderComponent } from '@coreui/angular';
+import { AccountService } from 'src/app/services/account.service';
 
 @Component({
   selector: 'app-default-header',
@@ -14,7 +16,13 @@ export class DefaultHeaderComponent extends HeaderComponent {
   public newTasks = new Array(5)
   public newNotifications = new Array(5)
 
-  constructor(private classToggler: ClassToggleService) {
+  constructor(private classToggler: ClassToggleService,
+    public accountService: AccountService,
+    public router:Router) {
     super();
+  }
+
+  logout() {
+    this.accountService.logout();
   }
 }
